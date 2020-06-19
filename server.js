a3a092f97fc89737768cd3f68c26e3336d31cff4
Your Name <you@example.com>
2020-06-19 23:14:16 +0600

    API Security

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
-const errorHandler = require("./middleware/error");
+//Security packages
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+//
 
+const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
 //Load Env Variables
@@ -25,6 +33,28 @@ if (process.env.NODE_ENV === "development") {
 }
 //file upload
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 10000,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
